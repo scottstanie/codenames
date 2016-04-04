aadf1bd0902509ba138178f231f07e1cbb8f6470
scott <scott@cogolabs.com>
2016-04-04 11:29:12 -0400

    add models and db

--- codenames/models.py
+++ codenames/models.py
@@ -1,11 +1,58 @@
-import datetime
+import hashlib
+import time
 from django.db import models
+from django.contrib.auth.models import User
+
+
+def _createHash():
+    """This function generate 10 character long hash"""
+    hash = hashlib.sha1()
+    hash.update(str(time.time()))
+    return hash.hexdigest()[:10]
+
+
+class Word(models.Model):
+    text = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.text
 
 
 class Card(models.Model):
-    word = models.CharField(max_length=200)
-    color = models.CharField(max_length=32)
+    word = models.ForeignKey(Word)
+    color = models.ArrayField(models.IntegerField())
     chosen = models.BooleanField(default=False)
 
     def __unicode__(self):
-        return '%s: %s' % (self.name, self.id)
+        return self.word
+
+
+class Board(models.model):
+    game = models.ForeignKey('Game')
+    cards = models.ManyToManyField(Card)
+
+    def __unicode__(self):
+        return self.game
+
+
+class Game(models.Model):
+    unique_id = models.CharField(max_length=10, default=_createHash, unique=True)
+    url = models.URLField()
+    red_giver = models.ForeignKey(User, related_name='red_giver', default=1)
+    red_guesser = models.ForeignKey(User, related_name='red_guesser', default=1)
+    blue_giver = models.ForeignKey(User, related_name='blue_giver', default=1)
+    blue_guesser = models.ForeignKey(User, related_name='blue_guesser', default=1)
+    started_date = models.DateTimeField('date started', auto_now_add=True)
+    board = models.ForeignKey(Board)
+
+    def __unicode__(self):
+        return self.url
+
+
+class Clue(models.Model):
+    word = models.CharField(max_length=96)
+    number = models.IntegerField()
+    giver = models.ForeignKey(User, default=1)
+
+    def __unicode__(self):
+        return self.word
--- codenames/settings.py
+++ codenames/settings.py
@@ -77,6 +77,12 @@ WSGI_APPLICATION = 'codenames.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'codenames',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
